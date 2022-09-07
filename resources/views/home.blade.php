5a8c8f9f9993926103d5c7aa3ac0e86a511d456a
bigeppo <carlog91cast@gmail.com>
2022-09-07 17:08:58 +0200

    add css in app.css

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.main')
+
+@section('main-content')
+<div class="jumbotron">
+    <h2>current series</h2>
+</div>
+<div class="cards-container">
+    <div class="container slide-box p-4">
+        <div>
+            <div>
+                <img class="w-100" src="/public/assets/img/jumbotron.jpg" alt="">
+            </div>
+            <h3></h3>
+        </div>
+    </div>
+</div>
+<div class="product-container">
+    <div id="wrapper">
+        <div>
+            <div class="img-container">
+                <img src="" alt="">
+                <h3>
+                    
+                </h3>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
